794a8347581cad12993f71254a6afc264e099c89
Guilherme <guilherme.palma8@etec.sp.gov.br>
2021-09-13 15:13:11 -0300

    Feature: Criação do Banco de Dados dos Produtos

--- script.sql
+++ script.sql
@@ -35,6 +35,21 @@ CREATE TABLE if not exists seller(
   PRIMARY KEY(cnpj)
 );
 
+CREATE TABLE if not exists products(
+  id_product INT NOT NULL,
+  cnpj_seller VARCHAR(14) NOT NULL,
+  name VARCHAR(80) NOT NULL,
+  code_state_city INT NOT NULL,
+  code_address INT NOT NULL,
+  quantity INT NOT NULL,
+  description TEXT NOT NULL,
+  price DECIMAL(8,2) NOT NULL,
+  weight DECIMAL(8,2) NOT NULL,
+  heigth DECIMAL(8,2) NOT NULL,
+  width DECIMAL(8,2) NOT NULL,
+  PRIMARY KEY(id_product)
+);
+
 /* Criação das Chaves Estrangeiras */
 ALTER TABLE client
 ADD FOREIGN KEY (code_state_city) REFERENCES state_city(code_state_city);
@@ -45,6 +60,9 @@ ALTER TABLE seller
 ADD FOREIGN KEY (code_state_city) REFERENCES state_city(code_state_city);
 ALTER TABLE seller
 ADD FOREIGN KEY (code_address) REFERENCES address(code_address);
+
+ALTER TABLE products
+ADD FOREIGN KEY (cnpj_seller) REFERENCES seller(cnpj);
 
 INSERT INTO state_city(code_state_city, city, state) VALUES
 (1, "São Paulo", "SP"),
@@ -92,3 +110,10 @@ SELECT * FROM seller;
 /*Verificar se Os Codigos da Cidade foram colocados corretamente com os Codigos de Endereço*/
 SELECT * FROM client ORDER BY code_state_city ASC;
 SELECT * FROM seller ORDER BY code_state_city ASC;
+
+
+INSERT INTO products(id_product,cnpj_seller,name,code_state_city,code_address,quantity,description,price,weight,heigth,width)VALUES
+(1, "18984552000147", "Televisão 60 Polegadas - 4K", 2, 3,10,"Por meio da Televisão de 60 Polegadas é possivel ter uma melhor experencias ao assistir programas,series e filmes. A Qualidade em 4K permite que as cores sejam mais vivas e intensas.", 8000.00, 19.00,857.00, 1368.00),
+(2, "12345679000123", "Mesa de Jantar - 6 Lugares", 2, 3,20,"Esta mesa pode atender 6 Pessoas com conforto. Mesa de Madeira Bruta, feita a partir de arvores reflorestavel e com baixa emissão de Carbono na sua produção", 8010.80, 24.9,79.6, 160.00),
+(3, "90896136000114", "Arte Digital - Desenho por Encomenda", 4, 5,40,"Encomende um desenho a partir de uma foto", 5, 1, 29.7, 21);
+SELECT * FROM products;
